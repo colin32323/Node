a7b5f53b9900e18715743620f08502dda16f4e17
Colin Charles <colincharles1204@gmail.com>
2025-03-24 00:21:04 +0530

    Minor changes

--- app.js
+++ app.js
@@ -8,8 +8,10 @@ const app = express();
 // Middleware
 app.use(express.json());
 
+// Routes
 app.use("/api", mainRouter);
 
+// Start server
 app.listen(process.env.PORT, () => {
     console.log("Server running on port ", process.env.PORT);
 });
